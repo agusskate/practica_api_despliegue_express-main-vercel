4cef645ce7f6182ca0d7650b231ad1af78c1a4c3
topocoptero <agus5048@gmail.com>
2025-01-20 23:09:50 +0100

    modified:   api/index.js

--- api/index.js
+++ api/index.js
@@ -9,23 +9,3 @@ app.listen(port, () => {
 
 
 
-// api/index.js
-const express = require('express');
-const router = express.Router(); // Usamos router de express
-
-// Definir rutas
-router.get('/users', async (req, res) => {
-  try {
-    const collection = database.collection('alumnos');
-    const users = await collection.find().toArray();
-    res.json(users);
-  } catch (error) {
-    console.error('Error al obtener los usuarios:', error);
-    res.status(500).json({ error: 'Error al obtener los usuarios' });
-  }
-});
-
-// Puedes agregar más rutas aquí si es necesario
-
-// Exportar el enrutador
-module.exports = router;  
